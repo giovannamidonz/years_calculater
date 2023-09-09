8392f44aad1f3c00325b2cc8db2a2c9757c3f0e4
Marllyson Alves <115300307+Marllysonnn@users.noreply.github.com>
2023-09-09 00:39:44 -0300

    Update api.js

--- api.js
+++ api.js
@@ -1,23 +1,62 @@
+const dataAtual = new Date();
+const dia = dataAtual.getDate();
+const mes = dataAtual.getMonth() + 1;
+const ano = dataAtual.getFullYear();
+
+const numdiaInput = document.getElementById('numdia');
+const nummesInput = document.getElementById('nummes');
+const anoInput = document.getElementById('ano');
+
+  numdiaInput.addEventListener('input', function() {
+    let numdia = numdiaInput.value.replace(/\D/g, '');
+    numdia = numdia.substring(0, 2);
+    numdiaInput.value = numdia;
+    if (numdia.length === 2) {
+      nummesInput.focus();
+    }
+  });
+  nummesInput.addEventListener('input', function() {
+    let nummes = nummesInput.value.replace(/\D/g, '');
+    nummes = nummes.substring(0, 2);
+    nummesInput.value = nummes;
+    if (nummes.length === 2) {
+      anoInput.focus();
+    }
+  });
+
 function subtracao(){
-  var ano = document.getElementById("ano");
+  var numano = document.getElementById("ano");
   var anoResult = document.getElementById("anor");
-  var anonum = parseFloat(ano.value);
-  var result_ano = 2023 - anonum;
-  anoResult.textContent = result_ano;
-  
+  var anonum = parseFloat(numano.value);
+  var result_ano = ano - anonum;
   var nummes = document.getElementById("nummes");
   var mesResult = document.getElementById("mesr");
   var mesnum = parseFloat(nummes.value);
-  var result_mes = 9 - mesnum;
-  mesResult.textContent = result_mes;
-
-  var numdia = document.getElementById("numdia");
-  var diaResult = document.getElementById("diar");
-  var dianum = parseFloat(numdia.value);
-  var result_dia = 8 - dianum;
-  diaResult.textContent = result_dia;
-
-  
-}
-
+  var result_mes = mes - mesnum;
+    if(result_mes < 1){
+      mesResult.textContent = result_mes + 11;
+      anoResult.textContent = result_ano - 1;
+    }
+    else{
+      mesResult.textContent = result_mes;
+      anoResult.textContent = result_ano;
+    }
+    var numdia = document.getElementById("numdia");
+    var diaResult = document.getElementById("diar");
+    var dianum = parseFloat(numdia.value);
+    var result_dia = dia - dianum;
+    if(result_dia < 0){
+      diaResult.textContent = 31 - (-1 * result_dia);
+    }
+    else{
+    diaResult.textContent = result_dia;
+    }
+  }
 document.getElementById("img").addEventListener("click", subtracao);
+const anoEnter = document.getElementById('ano');
+anoEnter.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    subtracao();
+  }
+});
